58d9cac7abbfffdff1dafdef96dba77f07ff95db
Di Giraldo <74432513+digiraldo@users.noreply.github.com>
2023-12-03 16:06:19 -0500

    Update usr.sh

--- usr.sh
+++ usr.sh
@@ -67,7 +67,7 @@ Print_Style "Agregando $usu como administrador" "$GREEN"
 sleep 3s
 # sed -i '$usu    ALL=(ALL:ALL) ALL' /etc/sudoers
 
-sudo sed -i '$usu ALL=(ALL:ALL) ALL'  /etc/sudoers
+sudo sed -i -e '$a $usu ALL=(ALL:ALL) ALL'  /etc/sudoers
 # sed -i '/$usu ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
 # sed -i '$a $usu ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
 # sed -n "/$usu ALL=(ALL) NOPASSWD: ALL/p" /etc/sudoers
